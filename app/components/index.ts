890c6f2854c9eb0ee17c39c393c70f6398ca8c36
root <root@autobots>
2023-04-23 22:35:02 +0600

    Creating the login page

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,3 +1,4 @@
 import Button from "./ui/Button";
+import Providers from "./Providers";
 
-export { Button };
+export { Button, Providers };
---
+++ app/lib/auth.ts
@@ -0,0 +1,47 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
+
+import { db } from "@/app/lib/db";
+
+export const authOptions: NextAuthOptions = {
+   adapter: UpstashRedisAdapter(db),
+   session: { strategy: "jwt" },
+   pages: { signIn: "/login" },
+   providers: [
+      GoogleProvider({
+         clientId: process.env.GOOGLE_CLIENT_ID as string,
+         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      }),
+   ],
+   callbacks: {
+      async jwt({ token, user }) {
+         const dbUser = (await db.get(`user:${token.id}`)) as User | null;
+
+         if (!dbUser) {
+            token.id = user!.id;
+            return token;
+         }
+
+         return {
+            id: dbUser.id,
+            name: dbUser.name,
+            email: dbUser.email,
+            picture: dbUser.image,
+         };
+      },
+      async session({ session, token }) {
+         if (token) {
+            session.user.id = token.id;
+            session.user.name = token.name;
+            session.user.email = token.email;
+            session.user.image = token.picture;
+         }
+
+         return session;
+      },
+      redirect() {
+         return "/dashboard";
+      },
+   },
+};
