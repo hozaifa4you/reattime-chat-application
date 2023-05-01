b3b69b8fa4e26f61522b52aeb845ca6fecb7fc9f
yousuf4you <yousuf360.ya@gmail.com>
2023-05-01 07:17:00 +0600

    small fix with fetch friends

---
+++ app/api/message/send/route.ts
@@ -0,0 +1,57 @@
+import { fetchRedis } from "@/app/helper/redis";
+import { authOptions } from "@/app/lib/auth";
+import { db } from "@/app/lib/db";
+import { Message, messageValidator } from "@/app/lib/validations/message";
+import { nanoid } from "nanoid";
+import { getServerSession } from "next-auth";
+import { z } from "zod";
+
+export async function POST(req: Request) {
+   try {
+      const { text, chatId }: { text: string; chatId: string } =
+         await req.json();
+      const session = await getServerSession(authOptions);
+      if (!session) return new Response("unauthorized 1", { status: 401 });
+
+      const [userId1, userId2] = chatId.split("--");
+
+      if (session.user.id !== userId1 && session.user.id !== userId2)
+         return new Response("unauthorized 2", { status: 401 });
+
+      const friendId = session.user.id === userId1 ? userId2 : userId1;
+      const friendList = (await fetchRedis(
+         "smembers",
+         `user:${session.user.id}:friends`
+      )) as string[];
+      const isFriend = friendList.includes(friendId);
+
+      if (!isFriend) return new Response("unauthorized 3", { status: 401 });
+
+      const rawSender = (await fetchRedis(
+         "get",
+         `user:${session.user.id}`
+      )) as string;
+      const sender = JSON.parse(rawSender) as User;
+
+      // send to database
+      const timestamp = Date.now();
+      const messageData: Message = {
+         id: nanoid(),
+         senderId: session.user.id,
+         text,
+         timestamp,
+      };
+      const message = messageValidator.parse(messageData);
+      await db.zadd(`chat:${chatId}:messages`, {
+         score: timestamp,
+         member: JSON.stringify(message),
+      });
+
+      return new Response("OK");
+   } catch (error) {
+      if (error instanceof Error || error instanceof z.ZodError) {
+         return new Response(error.message, { status: 500 });
+      }
+      return new Response("Internal server error", { status: 500 });
+   }
+}
--- app/helper/friends-by-id.ts
+++ app/helper/friends-by-id.ts
@@ -1,16 +1,24 @@
 import { fetchRedis } from "./redis";
 
 export const getFriendsById = async (userId: string): Promise<User[]> => {
-   const friendsIds = (await fetchRedis(
+   const friendIds = (await fetchRedis(
       "smembers",
       `user:${userId}:friends`
    )) as string[];
 
-   const friends = await Promise.all(
-      friendsIds.map(async (x) => {
-         const friend = (await fetchRedis("get", `user:${userId}`)) as string;
+   // const friends = await Promise.all(
+   //    friendsIds.map(async (x) => {
+   //       const friend = (await fetchRedis("get", `user:${userId}`)) as string;
 
-         return JSON.parse(friend) as User;
+   //       return JSON.parse(friend) as User;
+   //    })
+   // );
+
+   const friends = await Promise.all(
+      friendIds.map(async (friendId) => {
+         const friend = (await fetchRedis("get", `user:${friendId}`)) as string;
+         const parsedFriend = JSON.parse(friend) as User;
+         return parsedFriend;
       })
    );
 
