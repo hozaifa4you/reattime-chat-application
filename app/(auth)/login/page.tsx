3b9f8936548c8eea23639be3ec0a22d8aac75938
yousuf4you <yousuf360.ya@gmail.com>
2023-05-03 08:20:18 +0600

    added logo in login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -4,6 +4,7 @@ import { signIn } from "next-auth/react";
 import { toast } from "react-hot-toast";
 
 import { Button } from "@/app/components";
+import { Icons } from "@/app/components/Icons";
 
 const Login = () => {
    const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -25,7 +26,7 @@ const Login = () => {
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
          <div className="w-full flex flex-col items-center max-w-md space-y-8">
             <div className="flex flex-col items-center gap-8">
-               Logo
+               <Icons.Logo className="h-8 w-auto text-indigo-600" />
                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
                   Sign into your account
                </h2>
@@ -75,3 +76,4 @@ const Login = () => {
 };
 
 export default Login;
+
